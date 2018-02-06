25f741e110b647bb931f5d6dfdf3bdf585d73ed5
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-06 11:40:00 +0100

    Add files via upload

---
+++ edad.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+#Jandry Joel Aguilar Serrano
+#05/02/18
+#Star Wars: Identifícate
+
+#Preguntemos por el nombre
+nombre=raw_input("Indentifíquese: ")
+	if ((nombre=="Yoda") 
+		print "Bienvenido. Que la fuerza te acompañe"	
+	else:
+#Si es Darth Vader, también le dejaremos pasar
+		if(nombre == "Darth Vader") 
+			print "Hola padre"
+#Como la identificación no coincide, le denegamos el paso
+		else:
+			print "Acceso denegado")
+			 
+#END
