60815e1f9d7e57b02da75757636ffaf1b83714d4
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-08 12:04:27 +0100

    Create ejercicio-pares.py

---
+++ PythonSMX2/ejercicio-pares.py
@@ -0,0 +1,11 @@
+
+#coding: utf8
+#Jandry Joel Aguilar Serrano
+#08/02/18
+
+num=input("Introduzca el número: ")
+
+if num % 2 == 0:
+    print "Qué bonito número par"
+else :
+    print "Qué número más vulgar."
