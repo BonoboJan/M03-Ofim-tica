828197477b95e497f9785a9050669fa57cd92ebd
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-08 12:04:03 +0100

    Create ejercicio-edad-3.py

---
+++ PythonSMX2/ejercicio-edad-3.py
@@ -0,0 +1,9 @@
+
+#coding: utf8
+# Jandry Joel Aguilar Serrano
+# 08/02/18
+
+edad=input("Introduzca la edad: ")
+
+if (edad>=18 and edad<=23) or (edad ==17) :
+    print "Puede entrar en la sesión de jóvenes."
