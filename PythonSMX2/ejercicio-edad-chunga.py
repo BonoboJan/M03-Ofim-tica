f1a1de7cd573b01f0e3158dd5f535056daf61732
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-08 12:05:01 +0100

    Create ejercicio-edad-chunga.py

---
+++ PythonSMX2/ejercicio-edad-chunga.py
@@ -0,0 +1,9 @@
+
+#coding: utf8
+#Jandry Joel Aguilar Serrano
+#08/02/18
+
+num=input("Introduzca el número: ")
+
+if(num % 2 == 0) or (num>=-10 and num<=40) or (num<0) :
+    print "Cumple la condición chunga"
