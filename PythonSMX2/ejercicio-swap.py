1c04d0d81310e479c8b2d80cf478c8f72d403a00
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-08 12:05:19 +0100

    Add files via upload

---
+++ PythonSMX2/ejercicio-swap.py
@@ -0,0 +1,14 @@
+
+#coding: utf8
+#Jandry Joel Aguilar Serrano
+#08/02/18
+
+mano_der="movil"
+mano_izq="bocata"
+mano_extra=mano_der
+mano_der=mano_izq
+mano_izq=mano_extra
+
+print "Mano derecha: " , mano_der
+print "Mano izquierda: " , mano_izq
+print "Mano extra: " , mano_extra
