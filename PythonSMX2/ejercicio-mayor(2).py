1dd555f8d93e9fa2b34b656d88f26135221616da
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-02-22 09:38:28 +0100

    Create ejercicio-mayor(2).py

---
+++ PythonSMX2/ejercicio-mayor(2).py
@@ -0,0 +1,13 @@
+#coding: utf8
+#Jandry Joel Aguilar Serrano
+#21/02/18
+
+num1=input("Introduzca el primer número: ")
+num2=input("Introduzca el segundo número: ")
+
+if num1>num2 :
+    print "El número 1 es mayor"
+elif num1==num2:
+    print "Son iguales"
+else:
+    print "El número 2 es mayor"
