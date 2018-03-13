f1d67b97e4d6236f6efd5c6dd01627d334f5a0d8
BonoboJan <36151335+BonoboJan@users.noreply.github.com>
2018-03-13 11:39:32 +0100

    Update ejercicio-bucle-sumar.py

--- Bucles/ejercicio-bucle-sumar.py
+++ Bucles/ejercicio-bucle-sumar.py
@@ -2,12 +2,14 @@
 # Inicializaciones
 salir = "N"
 num=1
+suma = 0
 while ( salir=="N" ):
     # Hago cosas
-    print num, "+", num
+    print num, "+" , 
     # Incremento
+    suma = suma + num
     num= num +1
     # Activo indicador de salida si toca
     if ( num>5 ): # Condición de salida
-        print "="
+        print "=" , suma
         salir = "S"
